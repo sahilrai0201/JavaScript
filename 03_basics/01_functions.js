db1b7c71d9c939574d82a3113626aa9a971bb504
sahilrai0201 <sahilatwork21@gmail.com>
2026-07-09 17:50:51 +0530

    Refactor function examples and enhance documentation for clarity and consistency

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,75 +1,326 @@
-function sayMyName(){
-    console.log("S")
-    console.log("A")
-    console.log("H")
-    console.log("I")
-    console.log("L")
-}
-// sayMyName()
+// ===============================
+// FUNCTIONS IN JAVASCRIPT
+// ===============================
 
+// Function Declaration
+// A function is a block of reusable code.
+// It executes only when it is called.
+
+function sayMyName() {
+    console.log("S");
+    console.log("A");
+    console.log("H");
+    console.log("I");
+    console.log("L");
+}
+
+// Function Call (Invocation)
+// sayMyName();
+
+/*
+Output
+
+S
+A
+H
+I
+L
+*/
+
+
+// ===============================
+// FUNCTION PARAMETERS & ARGUMENTS
+// ===============================
+
+// Parameters
+// Variables written while defining a function.
+
+// Arguments
+// Actual values passed while calling the function.
+
+// Example
 
 // function addTwoNumbers(number1, number2){
 //     console.log(number1 + number2);
 // }
+
+// addTwoNumbers(3,6)
+
+// number1 and number2 → Parameters
+// 3 and 6 → Arguments
+
+
+// JavaScript automatically performs type coercion.
+
 // addTwoNumbers(3, 6)
+// Output
+// 9
+
 // addTwoNumbers(3, "6")
+// Output
+// 36
+
 // addTwoNumbers(3, "a")
+// Output
+// 3a
+
 // addTwoNumbers(3, null)
+// Output
+// 3
 
 
-function addTwoNumbers(number1, number2){
-    let result = number1 + number2
-    return result
+// ===============================
+// RETURN STATEMENT
+// ===============================
+
+// return sends a value back to the function call.
+// Code after return never executes.
+
+function addTwoNumbers(number1, number2) {
+
+    let result = number1 + number2;
+
+    return result;
 }
-const result = addTwoNumbers(3, 6)
-// console.log("Result : ", result)
+
+const result = addTwoNumbers(3, 6);
+
+// console.log("Result :", result);
+
+/*
+Output
+
+Result : 9
+*/
 
 
-function loginUserMessage(username = "Sam"){
-    if(username === undefined){
-        console.log("Please enter a username!")
-        return
+// ===============================
+// DEFAULT PARAMETERS
+// ===============================
+
+// If no argument is passed,
+// the default value is used.
+
+function loginUserMessage(username = "Sam") {
+
+    // This condition will never run
+    // when a default value is provided.
+
+    if (username === undefined) {
+        console.log("Please enter a username!");
+        return;
     }
-    return `${username} just logged in!`
-}
-// console.log(loginUserMessage())
-// console.log(loginUserMessage("Sahil Rai"))
 
+    return `${username} just logged in!`;
+}
+
+// console.log(loginUserMessage());
+
+/*
+Output
+
+Sam just logged in!
+*/
+
+
+// console.log(loginUserMessage("Sahil Rai"));
+
+/*
+Output
+
+Sahil Rai just logged in!
+*/
+
+
+// If there is NO default value
+
+/*
+function loginUserMessage(username){
+
+    if(!username){
+        console.log("Please enter a username");
+        return;
+    }
+
+    return `${username} just logged in`;
+}
+*/
+
+
+// ===============================
+// REST OPERATOR (...)
+// ===============================
+
+// Rest Operator collects multiple values
+// into a single array.
 
 // function calculateCartPrice(num1){
-//     return num1
+//     return num1;
 // }
-// console.log(calculateCartPrice(2))
-// console.log(calculateCartPrice(200, 400, 500))
+
+// console.log(calculateCartPrice(200));
+// Output
+// 200
+
+// console.log(calculateCartPrice(200,400,500));
+// Output
+// 200
+
+// Only the first argument is received.
+
+
+// Using Rest Operator
 
 // function calculateCartPrice(...num1){
-//     return num1
+//     return num1;
 // }
-// console.log(calculateCartPrice(200, 400, 500))
 
-function calculateCartPrice(val1, val2, ...num1){
-    return num1
+// console.log(calculateCartPrice(200,400,500));
+
+/*
+Output
+
+[200,400,500]
+*/
+
+
+// Mixing normal parameters and Rest Operator
+
+function calculateCartPrice(val1, val2, ...num1) {
+
+    return num1;
 }
-// console.log(calculateCartPrice(200, 400, 500, 2000))
 
+// console.log(calculateCartPrice(200,400,500,2000));
+
+/*
+Explanation
+
+val1 = 200
+
+val2 = 400
+
+num1 = [500,2000]
+
+Output
+
+[500,2000]
+*/
+
+
+// ===============================
+// PASSING OBJECTS TO FUNCTIONS
+// ===============================
+
+// Creating an object
 
 const user = {
-    username : "Sahil",
-    price : 199
+    username: "Sahil",
+    price: 199
+};
+
+
+// Function receiving an object
+
+function handleObject(anyobject) {
+
+    console.log(
+        `Username is ${anyobject.username} and price is ${anyobject.price}`
+    );
 }
-function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
-}
-// handleObject(user)
+
+
+// Passing an existing object
+
+// handleObject(user);
+
+
+// Passing an object directly
+
 handleObject({
-    username : "Sam",
-    price : 399
-})
+    username: "Sam",
+    price: 399
+});
+
+/*
+Output
+
+Username is Sam and price is 399
+*/
 
 
-const myNewArray = [200, 400, 600, 800]
-function returnSecondValue(getArray){
-    return getArray[1]
+// ===============================
+// PASSING ARRAYS TO FUNCTIONS
+// ===============================
+
+// Creating an array
+
+const myNewArray = [200, 400, 600, 800];
+
+
+// Function receiving an array
+
+function returnSecondValue(getArray) {
+
+    return getArray[1];
 }
-// console.log(returnSecondValue(myNewArray))
-console.log(returnSecondValue([200, 400, 1000, 1200]))
+
+
+// Passing an existing array
+
+// console.log(returnSecondValue(myNewArray));
+
+/*
+Output
+
+400
+*/
+
+
+// Passing an array directly
+
+console.log(returnSecondValue([200, 400, 1000, 1200]));
+
+/*
+Output
+
+400
+*/
+
+
+// ===============================
+// SUMMARY
+// ===============================
+
+/*
+
+1. Function
+   -> Reusable block of code.
+
+2. Function Call
+   -> Executes the function.
+
+3. Parameters
+   -> Variables in function definition.
+
+4. Arguments
+   -> Actual values passed during function call.
+
+5. return
+   -> Sends a value back to the caller.
+
+6. Default Parameter
+   -> Used when no argument is passed.
+
+7. Rest Operator (...)
+   -> Collects multiple arguments into an array.
+
+8. Objects can be passed directly to functions.
+
+9. Arrays can also be passed directly to functions.
+
+10. JavaScript allows functions to accept
+    numbers, strings, objects, arrays,
+    functions, and even other functions.
+
+*/
